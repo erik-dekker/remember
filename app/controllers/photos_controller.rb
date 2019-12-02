8c3106ea238fbc99f91e569096fab41f9c11012c
Erik Dekker <erik@erikdekker.net>
2019-12-02 21:38:37 +0100

    Fixes uploading photos to correct album id

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,18 @@
 class PhotosController < ApplicationController
   def new
-
+    @photo = Photo.new
+    @album = Album.find(params[:album_id])
   end
+
+  def create
+    @photo = Photo.new
+    @photo.album_id = params[:album_id]
+    @photo.save
+    redirect_to album_path(@photo.album_id)
+  end
+
+  private
+    def photo_params
+      params.require(:photo).permit(:title, :description, :img_data)
+    end
 end
